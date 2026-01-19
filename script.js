591f4245998322d039c0f7b619fd5a8dbbdf4dbb
MochaSm <130510590+MochaSm@users.noreply.github.com>
2026-01-19 09:46:05 -0800

    asdads

---
+++ script.js
@@ -0,0 +1,35 @@
+let navMobile = document.getElementById('nav-mobile');
+let navDesktop = document.getElementById('nav-desktop');
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, function(error) {
+        console.log('Service Worker registration failed:', error);
+      });
+    });
+  }
---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/dash.html',
+          '/style.css',
+          '/script.js',
+          '/icons/icon-192x192.png',
+          '/icons/icon-512x512.png',
+          '/static/manifest.json',
+          '/static/icon512_maskable.png',
+          '/static/icon512_rounded.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
